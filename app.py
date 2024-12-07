5e63502c313c8646fd8159c11cc4b765da54ebc8
Sathyam <139795738+iSathyam31@users.noreply.github.com>
2024-12-08 02:01:08 +0530

    final changes

--- app.py
+++ app.py
@@ -9,9 +9,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Langsmith Tracking
-os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
-os.environ["LANGCHAIN_TRACING_V2"] = "true"
-os.environ["LANGCHAIN_PROJECT"] = "Enhanced Q&A Chatbot With Ollama"
+# os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
+# os.environ["LANGCHAIN_TRACING_V2"] = "true"
+# os.environ["LANGCHAIN_PROJECT"] = "Enhanced Q&A Chatbot With Ollama"
 
 # Prompt template
 template = """
